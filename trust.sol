adf65f3015af5110a46061ae22b1e293747644ec
tkernell <tpkernell@gmail.com>
2017-06-18 00:55:55 -0500

    Update trust.sol

--- trust.sol
+++ trust.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.10;
  WARNING - THIS CONTRACT IS IN TEST MODE. IT SHOULD NOT BE USED TO STORE REAL ETHER!
 
  This is an Ethereum smart contract that acts as a trust. A user transfers ether
- to the contract and then adds some people who he or she trusts. Then for a period
+ to the contract and then adds some people whom he or she trusts. Then for a period
  of time the initial user has no control over the funds and the trusted new owners
  have the ability to move the ether as long as every single one of them agrees.
  After a certain amount of time, the original user regains control of the contract
@@ -13,19 +13,21 @@ pragma solidity ^0.4.10;
 */
 
 contract Trust {
-    address public beneficiary;
-    address public initialOwner;
-    uint public deadline;
-    uint public setupDeadline;
-    bool trustClosed = false;
-    uint nonce;
+    address public beneficiary;			// Gains ownership of any funds left after deadline
+    address public initialOwner;		// Creates contract & sets trust owners
+    uint public deadline;			// Determines when beneficiary gains control
+    uint public setupDeadline;			// Determines when trust owners gain control
+    uint public board;				// Keeps track of number of trustees
     
-    struct TrustOwners {
-        address splitOwner;
-    }
     
-    mapping (uint => TrustOwners) public owners;
+	// The 'votes' array handles votes to move funds out of trust & into other account(s)
+    mapping (uint => address) public trustees;
     mapping (address => mapping (address => uint256)) public votes;
+    
+    event TrusteeAdded(address trustee);
+    event TrusteeVotedToSendFunds(address trustee, address recipient, uint256 amount);
+    event TrusteeWithdrewFunds(address trustee, uint256 amount);
+    event BeneficiaryWithdrew(address beneficiary);
     
     // only initial owner
 	modifier onlyInitialOwner {
@@ -33,80 +35,89 @@ contract Trust {
 	    _;
 	}
 	
-	modifier onlyTrustOwner {
-	    bool isTrustOwner = false;
+	// Limits activites to only trustees
+	modifier onlyTrustee {
+	    bool isTrustee = false;
 	    
-	    for (uint i = 0; i < nonce; i ++) { 
-	        if (owners[i].splitOwner == msg.sender) isTrustOwner = true;
-	        
+	    for (uint i = 0; i < board; i++) { 
+	        if (trustees[i] == msg.sender) isTrustee = true;
 	    }
-	    require(isTrustOwner == true);
+		
+	    require(isTrustee == true);
 	    _;
 	}
+	
+	modifier duringSetup {
+		require(now <= setupDeadline);
+		_;
+	}
+	
+	modifier duringTrust {
+		require(now >= setupDeadline);
+		require(now <= deadline);
+		_;
+	}
     
+    // Setup function
     function Trust(
-        address futureBeneficiary,
+        address futureBeneficiary,	
         uint setupDurationInMinutes,
         uint durationInMinutes
         ) {
             beneficiary = futureBeneficiary;
-            setupDeadline = setupDurationInMinutes * 1 minutes;
+            setupDeadline = now + setupDurationInMinutes * 1 minutes;
             deadline = setupDeadline + durationInMinutes * 1 minutes;
             initialOwner = msg.sender;
         }
-        
-    function addOwner(address newOwner) onlyInitialOwner {
-            if (now > setupDeadline) throw;
-            owners[nonce].splitOwner = newOwner;
-            nonce++;
+    
+    function addTrustee(address newTrustee) onlyInitialOwner duringSetup {
+            trustees[board] = newTrustee;
+            board++;
+            TrusteeAdded(newTrustee);
         }
         
-    function ownerVoteToSend(address _to, uint256 _value) onlyTrustOwner {
-        if (now > deadline) throw;
-        if (now < setupDeadline) throw;
+    function trusteeVoteToSend(address _to, uint256 _value) onlyTrustee duringTrust {
         if (_to == 0x0) throw;
         if (this.balance < _value) throw;
         votes[msg.sender][_to] = _value;
+        TrusteeVotedToSendFunds(msg.sender, _to, _value);
     }
-    
-    
+   
     // Withdraws funds after trust owners unanimously agree
-    function withdrawAfterVotes(uint256 _value) onlyTrustOwner {
-        if (now > deadline) throw;
-        if (now < setupDeadline) throw;
+    function withdrawAfterVotes(uint256 _value) onlyTrustee duringTrust {
         if (this.balance < _value) throw;
         
-        uint8 iter = 1;
-        uint256 prevValue = votes[owners[iter].splitOwner][msg.sender];
+        uint256 prevValue = votes[trustees[0]][msg.sender];
         uint256 prevValue2;
         
-        if (prevValue != _value) {
-            throw;
-        }
+        require(prevValue == _value);
+     
+		for (uint i=1; i<board; i++) {
+			prevValue2 = votes[trustees[i]][msg.sender];
+			require(prevValue2 == prevValue);
+			prevValue = prevValue2;
+		}
         
-        while (iter < nonce) {
-            prevValue2 = votes[owners[iter].splitOwner][msg.sender];
-            if (prevValue2 != prevValue) {
-                throw;
-            }
-            prevValue = prevValue2;
-            iter++;
-        }
-        
+		for (i=0; i<board; i++) {
+			votes[trustees[i]][msg.sender] = 0;
+		}
+		TrusteeWithdrewFunds(msg.sender, _value);
         msg.sender.transfer(prevValue);
     }
     
+    // Beneficiary can withdraw in the end
     function beneficiaryWithdraw() onlyInitialOwner {
         if (now < deadline) throw;
-        beneficiary.transfer(this.balance);
+        require(beneficiary.send(this.balance));
+        BeneficiaryWithdrew(msg.sender); 
+        
     }
     
-    function() payable {
-    }
+    function() payable {}
     
     // clean up
-	function kill() onlyInitialOwner {
-	    selfdestruct(initialOwner);
-	}
+    function kill() onlyInitialOwner {
+        selfdestruct(initialOwner);
+    }
 }
 	
